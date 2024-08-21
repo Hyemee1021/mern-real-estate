834b04b5f59072b2997b4a61c05f2d6996cc319e
Hye Mee Park <hyemee1021@gmail.com>
2024-08-21 16:31:20 +1000

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
+app.use(cookieParser());
 //put mongoose string
 mongoose
   .connect(process.env.MONGO)
@@ -17,3 +23,5 @@ mongoose
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/listing", listingRouter);
